749b777888d8552b134a898b45058160010b07e1
UMOO <umer.idrees.1906@gmail.com>
2024-03-30 16:16:23 +0500

    Code improved for Quality Checks

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from joblib import dump, load
 
 app = Flask(__name__)
 accuracy = None
@@ -13,16 +12,12 @@ X = data.drop('Quality', axis=1)
 y = data['Quality']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 clf = RandomForestClassifier(random_state=42)
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
-
-dump(clf, 'random_forest_model.joblib')
-
 @app.route('/model_accuracy', methods=['GET'])
 def model_accuracy():
     if accuracy is not None:
@@ -33,5 +28,6 @@ def model_accuracy():
         return jsonify({
             'message': "The model's accuracy is not available."
         })
+
 if __name__ == '__main__':
     app.run(debug=True)
